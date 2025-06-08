3a6d2a759b5c450fa27d8df45fca41ca8c957ddb
rizkyadb <rizki.adityapramana21@gmail.com>
2025-06-09 01:06:11 +0700

    Deploy application to Netlify

---
+++ dist/assets/cursor-BE7WQJF9.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-Dm4rL_Da.js";import"./index-DF3b9ScI.js";import"./index.es-DVj0gn38.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
